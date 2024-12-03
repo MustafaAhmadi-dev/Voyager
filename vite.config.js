ea878d9863cd7ffa114a5dba4251da778f96318f
Mustafa Ahmadi <118629586+MustafaAhmadi-dev@users.noreply.github.com>
2024-12-03 18:59:08 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [checker({ typescript: false }), react()],
 })
